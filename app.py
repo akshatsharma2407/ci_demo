157f31fbac99501d4322b882fc3fafde257d0add
Akshat Sharma <akshatsharma5877@gmail.com>
2025-10-25 21:50:48 +0530

    linting issue solved

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-def add(a,b):
-    return a+b
+def add(a, b):
+    return a + b
 
 
+def sub(a, b):
+    return a - b
 
 
+result1 = add(1, 2)
+result2 = sub(2, 4)
 
-def sub(a,b): return a-b
-
-result1 = add(1,2)
-result2 = sub(2,4)
-
-print(result1(1,23),result2(4,2))
+print(result1(1, 23), result2(4, 2))
